b15068bb26f7f253a8ffd3ac96f5b3d8c2cd383a
2024559507-wq <2024559507@student.uitm.edu.my>
2026-05-27 14:51:05 +0800

    Add files via upload

---
+++ WebApplication3/src/java/ProfileServlet.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ProfileServlet")
+public class ProfileServlet extends HttpServlet  {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Collect form data
+        String name = request.getParameter("name");
+        String studentId = request.getParameter("studentId");
+        String program = request.getParameter("program");
+        String email = request.getParameter("email");
+        String hobbies = request.getParameter("hobbies");
+        String intro = request.getParameter("intro");
+
+        // Pass attributes to JSP
+        request.setAttribute("name", name);
+        request.setAttribute("studentId", studentId);
+        request.setAttribute("program", program);
+        request.setAttribute("email", email);
+        request.setAttribute("hobbies", hobbies);
+        request.setAttribute("intro", intro);
+
+        // Forward to JSP page
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/profile.jsp");
+        dispatcher.forward(request, response);
+    }
+}
